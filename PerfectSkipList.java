fab3dbf88dbc6f145ede7101ffe6457bd0f0673c
Rakesh Iyer <rakesh.iyer.n@gmail.com>
2017-07-23 19:18:19 -0700

    Fixes

--- PerfectSkipList.java
+++ PerfectSkipList.java
@@ -4,12 +4,36 @@ class PerfectSkipList {
     int level;
     Node start = new Node("", new Data());
     Node end = new Node("LARGESTKEYEVER", new Data());
+    int size;
 
+    int getSize() {
+        return size;
+    }
+
+    void incrementSize() {
+        size++;
+    }
+
+    void decrementSize() {
+        size--;
+    }
 
     static class DuplicateKeyException extends Exception {
         String exceptionString;
 
         DuplicateKeyException(String exceptionString) {
+            this.exceptionString = exceptionString;
+        }
+
+        public String toString() {
+            return exceptionString;
+        }
+    }
+
+    static class KeyNotFoundException extends Exception {
+        String exceptionString;
+
+        KeyNotFoundException(String exceptionString) {
             this.exceptionString = exceptionString;
         }
 
@@ -58,21 +82,34 @@ class PerfectSkipList {
         }
     }
 
-    Node search(String key) {
+    // either returns the node with the given key or the location after which to insert.
+    private Node searchInternal(String key) {
         Node node = start;
         int searchLevel = level;
 
         while (searchLevel >= 0) {
+            Node rightNode = node.links.get(searchLevel);
+
             if (node.getKey().equals(key)) {
                 return node;
-            } else if (node.getKey().compareTo(node.links.get(searchLevel).getKey()) >= 0) {
-                node = node.links.get(searchLevel);
+            } else if (rightNode.getKey().compareTo(key) <= 0) {
+                node = rightNode;
             } else {
                 searchLevel--;
             }
         }
 
-        return null;
+        return node;
+    }
+
+    Node search(String key) {
+        Node node = searchInternal(key);
+
+        if (node.getKey().equals(key)) {
+            return node;
+        } else {
+            return null;
+        }
     }
 
     int log(int length) {
@@ -90,53 +127,56 @@ class PerfectSkipList {
         return bit;
     }
 
-    void insert(Node ins, Node curr, Node next, int level) {
+    void insert(Node ins, Node curr, int level) {
+        Node next = curr.links.get(level);
+
         curr.links.set(level, ins);
         ins.links.set(level, next);
     }
 
-    void insert(Node ins) throws DuplicateKeyException {
-        // insert in lowest level, then simply create higher levels on 1/2 principle.
-        Node curr = start;
-        int length = 0;
-
-        while (curr != end) {
-            Node next = curr.links.get(0);
-
-            length++;
-            if (next.getKey().compareTo(ins.getKey()) > 0) {
-                insert(ins, curr, next, 0);
-                break;
-            } else if (next.getKey().compareTo(ins.getKey()) < 0) {
-                curr = next;
-            } else {
-                throw new DuplicateKeyException("Node " + ins + " has duplicate key");
-            }
-        }
-
-
-        // build all the higher levels again.
-        int maxLevel = log(length);
+    void rebuild() {
+        int maxLevel = log(getSize());
         for (int level = 1; level < maxLevel; level++) {
             Node prevLevelCurr = start;
             Node nextLevelCurr = start;
 
             while (prevLevelCurr != end) {
-                Node next = curr.links.get(level);
+                Node next = prevLevelCurr.links.get(level-1);
 
                 if (next == end) {
                     break;
                 }
 
-                insert(next, nextLevelCurr, end, level);
+                insert(next, nextLevelCurr, level);
                 nextLevelCurr = next;
-
-                prevLevelCurr = next.links.get(level);
+                prevLevelCurr = next.links.get(level-1);
             }
         }
     }
 
-    void delete(Node n) {
+    void insert(Node ins) throws DuplicateKeyException {
+        // insert in lowest level, then simply create higher levels on 1/2 principle.
+        Node prev = searchInternal(ins.getKey());
+        if (prev.getKey().equals(ins.getKey())) {
+            throw new DuplicateKeyException("Node " + ins + " has duplicate key");
+        }
+
+        insert(ins, prev, 0);
+        incrementSize();
+
+        // build all the higher levels again.
+        rebuild();
+    }
+
+    void delete(String key) throws KeyNotFoundException {
+        Node del = search(key);
+
+        if (del == null) {
+            throw new KeyNotFoundException("Cannot find node with key " + key);
+        }
+
+        // you will find the node at the highest level, so just follow lower link and delete from each level.
+        // need a doubly linked list for log n time deletion.
     }
 
     public static void main(String args[]) {
